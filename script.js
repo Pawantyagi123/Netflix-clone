2b5138ccb349a088b68bd374eb69bcc967a3a28e
pawantyagi123 <pawantyaginnl2002@gmail.com>
2024-03-09 22:46:40 +0530

    update

--- script.js
+++ script.js
@@ -24,46 +24,9 @@ const options = ["English", "Hindi", "French", "Japnes", "Chines"];
     // Append the select element to the dropdown container
     document.getElementById("dropdownContainer").appendChild(select);
 
-    document.addEventListener("DOMContentLoaded", function() {
-        var dropdown = document.getElementById("faqDropdown");
-        var dropdownContent = document.getElementById("faqDropdownContent");
-    
 
-        // Show/hide dropdown content when the dropdown is clicked
-        dropdown.addEventListener("click", function() {
-            dropdownContent.classList.toggle("show");
-           
-        });
-
-        // Hide dropdown content when clicking outside the dropdown
-        window.addEventListener("click", function(event) {
-            if (!dropdown.contains(event.target)) {
-                dropdownContent.classList.remove("show");
-            }
-        });
-    });
-
-
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     var dropdown = document.getElementById("faqDropdown");
-    
-    //     // Show/hide dropdown content when the dropdown is clicked
-    //     dropdown.addEventListener("click", function(event) {
-    //         var dropdownContent = document.getElementById("faqDropdownContent");
-    //         dropdownContent.classList.toggle("show");
-    //         event.stopPropagation(); // Prevent the click event from propagating to the window
-    //     });
-    
-    //     // Hide dropdown content when clicking outside the dropdown
-    //     window.addEventListener("click", function(event) {
-    //         var dropdownContent = document.getElementById("faqDropdownContent");
-    //         if (!dropdown.contains(event.target)) {
-    //             dropdownContent.classList.remove("show");
-    //         }
-    //     });
-    // });
     function toggleDescription(icon) {
-        var descriptionText = icon.nextElementSibling;
+        let descriptionText = icon.nextElementSibling;
         descriptionText.style.display = descriptionText.style.display === 'none' ? 'block' : 'none';
     }
 
